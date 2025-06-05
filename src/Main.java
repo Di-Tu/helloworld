507828f87281b31dcea0ff9062f9f1e048d42674
Ди <faraon_m@mail.ru>
2025-06-05 20:50:08 +0300

    Test1

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,7 @@ public class Main {
         System.out.print("  Как жизнь?");
         System.out.println("  Как здоровье?");
         System.out.println("Всё хорошо!");
+        System.out.println("Как твоя семья?");
+        System.out.println("Все здоровы, всё хорошо.");
     }
 }
